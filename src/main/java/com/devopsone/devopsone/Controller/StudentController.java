19a1d8132d3554e9b512b27ab144e9b7ae304ad5
Tin Soon Phei <soonphei@Tins-MacBook-Pro.local>
2022-04-29 14:58:52 +0800

    retrieve student by ID api added

--- src/main/java/com/devopsone/devopsone/Controller/StudentController.java
+++ src/main/java/com/devopsone/devopsone/Controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,10 @@ public class StudentController {
     public List<String> getAllStudents(){
         List<String> students = Arrays.asList("Tammy", "Johnson");
         return students;
+    }
+
+    @GetMapping("/student/{id}")
+    public String getStudent(@RequestParam int id){
+        return "ME";
     }
 }
